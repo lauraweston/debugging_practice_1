a1fbda80159599e3f6644cbe17ee07e45a17126f
Mary Rose Cook <mary@maryrosecook.com>
2016-11-10 14:08:53 +0000

    Correct tests

--- spec/verify/calculator_spec.rb
+++ spec/verify/calculator_spec.rb
@@ -1,31 +1,29 @@
 require 'spec_helper'
 
 describe Calculator do
-  before :each do
-    let(:calculator) { described_class.new }
-  end
+  let(:calculator) { described_class.new }
 
   describe "#add" do
     it "adds two numbers" do
-      assert_equal 5, @calculator.add(2, 3)
+      expect(calculator.add(2, 3)).to equal(5)
     end
   end
 
   describe "#subtract" do
     it "subtracts two numbers" do
-      assert_equal 5, @calculator.subtract(2, 3)
+      expect(calculator.subtract(2, 3)).to equal(-1)
     end
   end
 
   describe "#multiply" do
     it "multiplies two numbers" do
-      assert_equal 5, @calculator.multiply(2, 3)
+      expect(calculator.multiply(2, 3)).to equal(6)
     end
   end
 
   describe "#divide" do
     it "divides two numbers" do
-      assert_equal 5, @calculator.divide(2, 3)
+      expect(calculator.divide(2, 2)).to equal(1)
     end
   end
 end
