76a4d8a85df4ff29975e00c9df50d8f68ade6d45
Mary Rose Cook <mary@maryrosecook.com>
2016-11-28 11:15:35 +0000

    more

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,9 +7,17 @@ class Blackjack
     @player_hands = player_hands
   end
 
+  def self.valid_player_move?(move)
+    [:hit, :stand].include?(move)
+  end
+
+  def self.valid_player_count?(count)
+    count > 0
+  end
+
   def play_move(move)
     raise "Game is over" if winner?
-    raise "Invalid move" unless [:hit, :stand].include?(move)
+    raise "Invalid move" unless self.class.valid_player_move?(move)
 
     current_player_hand.send(move)
     current_player_hand.status
@@ -20,14 +28,18 @@ class Blackjack
   end
 
   def winner
-    @player_hands.select { |hand|
-      hand.status == :standing
-    }.sort { |hand_1, hand_2|
-      hand_2.score <=> hand_1.score
-    }
+    return nil if game_still_going?
+
+    @player_hands
+      .select { |hand| hand.status == :standing }
+      .sort { |hand_1, hand_2| hand_2.score <=> hand_1.score }[0]
   end
 
   private
+
+  def game_still_going?
+    @player_hands.any? { |hand| hand.status == :playing }
+  end
 
   def current_player_hand
     @player_hands[0]
--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -13,42 +13,50 @@ describe Blackjack do
   end
 
   describe "#play_move" do
-    let(:blackjack_hand) {
-      double(:blackjack_hand, status: :playing)
-    }
-    subject { described_class.new([blackjack_hand]) }
+    let(:playing_blackjack_hand) { double(:blackjack_hand,
+                                          status: :playing) }
 
     it "hits for the current player hand" do
-      allow(blackjack_hand).to receive(:hit)
+      subject = described_class.new([playing_blackjack_hand])
+      allow(playing_blackjack_hand).to receive(:hit)
       expect(subject.play_move(:hit)).to be(:playing)
     end
 
-    it "stick for the current player hand" do
-      allow(blackjack_hand).to receive(:stand)
+    it "stand for the current player hand" do
+      allow(playing_blackjack_hand).to receive(:stand)
+      subject = described_class.new([playing_blackjack_hand])
       expect { subject.play_move(:stand) }.to_not raise_error
     end
 
-    it "raises error if anything but stick or hit called" do
-      allow(blackjack_hand).to receive(:moo)
+    it "raises error if anything but stand or hit called" do
+      subject = described_class.new([playing_blackjack_hand])
+      allow(playing_blackjack_hand).to receive(:moo)
       expect { subject.play_move(:moo) }
         .to raise_error("Invalid move")
     end
 
     it "reports the player hand state after move made" do
-      allow(blackjack_hand).to receive(:hit)
-      allow(blackjack_hand).to receive(:status).and_return(:bust)
+      subject = described_class.new([playing_blackjack_hand])
+      allow(playing_blackjack_hand).to receive(:hit)
+      allow(playing_blackjack_hand)
+        .to receive(:status).and_return(:bust)
       expect(subject.play_move(:hit)).to be(:bust)
     end
 
     it "raises if hand player move raises" do
-      allow(blackjack_hand)
+      subject = described_class.new([playing_blackjack_hand])
+      allow(playing_blackjack_hand)
         .to receive(:hit).and_raise("Invalid player hand move")
       expect { subject.play_move(:hit) }
         .to raise_error("Invalid player hand move")
     end
 
     it "raises if already a winner" do
-      allow(blackjack_hand).to receive(:hit)
+      standing_blackjack_hand = double(:blackjack_hand,
+                                       status: :standing)
+      subject = described_class.new([standing_blackjack_hand])
+
+      allow(standing_blackjack_hand).to receive(:hit)
       expect { subject.play_move(:hit) }
         .to raise_error("Game is over")
     end
@@ -62,14 +70,43 @@ describe Blackjack do
     end
 
     it "returns standing player when other players bust" do
-      blackjack_hand_1 = double(:blackjack_hand, status: :standing)
-      blackjack_hand_2 = double(:blackjack_hand, status: :bust)
-      blackjack_hand_3 = double(:blackjack_hand, status: :bust)
+      blackjack_hand_1 = double(:blackjack_hand_1,
+                                status: :standing)
+      blackjack_hand_2 = double(:blackjack_hand_2,
+                                status: :bust)
+      blackjack_hand_3 = double(:blackjack_hand_3,
+                                status: :bust)
 
       subject = described_class.new([blackjack_hand_1,
                                      blackjack_hand_2,
                                      blackjack_hand_3])
       expect(subject.winner).to be(blackjack_hand_1)
+    end
+
+    it "returns top scoring player" do
+      blackjack_hand_1 = double(:blackjack_hand_1,
+                                status: :standing,
+                                score: 20)
+      blackjack_hand_2 = double(:blackjack_hand_2,
+                                status: :standing,
+                                score: 21)
+
+      subject = described_class.new([blackjack_hand_1,
+                                     blackjack_hand_2])
+      expect(subject.winner).to be(blackjack_hand_2)
+    end
+
+    it "returns no winner when a player still playing" do
+      blackjack_hand_1 = double(:blackjack_hand_1,
+                                status: :standing,
+                                score: 20)
+      blackjack_hand_2 = double(:blackjack_hand_2,
+                                status: :playing,
+                                score: 21)
+
+      subject = described_class.new([blackjack_hand_1,
+                                     blackjack_hand_2])
+      expect(subject.winner).to be_nil
     end
   end
 end
