9353af87686df69cdb1877cafa7f01df4aebc5e0
Laura Weston <laura.weston@outlook.com>
2016-11-30 16:32:16 +0000

    Change Hand#hit to raise error when bust? is true

--- lib/blackjack/hand.rb
+++ lib/blackjack/hand.rb
@@ -12,7 +12,7 @@ class Hand
 
   def hit(card_class=Card)
     raise "You're standing" if standing?
-    raise "You're bust" if standing?
+    raise "You're bust" if bust?
     card_list.add(card_class.random)
   end
 
